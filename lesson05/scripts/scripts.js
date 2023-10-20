ecb7eb775e55aea65d3bd22897efff054d1c0d0c
rmwdcsur <rmwdcsur@hotmail.com>
2023-10-19 22:49:19 -0300

    fixing basic chapter number validation

--- lesson05/scripts/scripts.js
+++ lesson05/scripts/scripts.js
@@ -6,19 +6,25 @@ const list = document.querySelector('#list');
 // Event listener for Add Chapter button
 button.addEventListener('click', () => {
     if (input.value != '') {
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
-        li.append(deleteButton);
-        list.append(li);
-        input.value = "";
-        // Event listener for Delete button
-        deleteButton.addEventListener('click', () => {
-            list.removeChild(li);
+        let chapter = getChapterNumber(input.value);
+        if (chapter > 0 && chapter < 64) {
+            const li = document.createElement('li');
+            const deleteButton = document.createElement('button');
+            li.textContent = input.value;
+            deleteButton.textContent = '❌';
+            li.append(deleteButton);
+            list.append(li);
+            input.value = "";
+            // Event listener for Delete button
+            deleteButton.addEventListener('click', () => {
+                list.removeChild(li);
+                input.focus();
+            });
             input.focus();
-        });
-        input.focus();
+
+        } else {
+            window.alert("You must include a chapter number or the chapter number is invalid!")
+        }
     }
     else {
         window.alert("You must type a Book and Chapter!");
@@ -32,3 +38,17 @@ input.addEventListener("keyup", function (event) {
         button.click();
     }
 });
+
+
+// Function to extract numbers
+function getChapterNumber(aString) {
+
+    // Using match with regEx
+    let matches = aString.match(/(\d+)/);
+
+    // Display output if number extracted
+    if (matches) {
+        //console.log(matches[0]);
+        return matches[0];
+    }
+};
