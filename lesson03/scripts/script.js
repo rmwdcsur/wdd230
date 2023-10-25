7d323e0571f4be318fd77ff6576c8f32c3b6fc58
rmwdcsur <rmwdcsur@hotmail.com>
2023-10-25 20:41:44 -0300

    applying grading feedback

---
+++ lesson03/scripts/script.js
@@ -0,0 +1,4 @@
+let oLastModif = document.lastModified;
+let currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
+document.getElementById("lastmodified").textContent = oLastModif;
