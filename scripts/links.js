f375f11ec96e24759f7ebd0e13557baeeb4813aa
rmwdcsur <rmwdcsur@hotmail.com>
2023-11-10 22:18:39 -0300

    updating json file

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,12 @@
 const baseURL = "https://rmwdcsur.github.io/wdd230/";
 
-const linksURL = "https://rmwdcsur.github.io/wdd230/data/links.json";
+// const linksURL = "https://rmwdcsur.github.io/wdd230/data/links.json";
+const linksURL = "../data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data.lessons);
+    // console.log(data.lessons);
     displayLinks(data.lessons);
 }
 
@@ -18,19 +19,18 @@ const displayLinks = (weeks) => {
         let listItem = document.createElement('li');
         listItem.textContent = "Week " + week.lesson + ": ";
         for (let i = 0; i < week.links.length; i++) {
-            console.log(week.links[i].title);
-            console.log(week.links[i].url);
+            // console.log(week.links[i].title);
+            // console.log(week.links[i].url);
             let alink = document.createElement('a');
             alink.href = week.links[i].url;
-            if (i == 1) {
+            if (i == 0) {
                 alink.text = week.links[i].title;
             } else {
-                alink.text = week.links[i].title + ' | ';
+                alink.text = ' | ' + week.links[i].title;
             }
             listItem.appendChild(alink);
 
         }
         list.appendChild(listItem);
-        // console.log(week.links[0]);
     });
 }
